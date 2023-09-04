884afbf39eb57347f57b0ed4308717adfbf82aa1
hv122 <hv122@ic.ac.uk>
2023-09-04 22:08:52 +0100

    - fixing weather

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import CurrentWeather from './src/components/CurrentWeather';
+import UpcomingWeather from './src/components/UpcomingWeather';
 /* view is the equivalent of div
 safe area view is to avoid notches and holepunches 
 when using images from their address, you have to specify their dimensions */
@@ -7,35 +9,23 @@ when using images from their address, you have to specify their dimensions */
 const App = () => {
 
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View styles={styles.container}>
-        <Text style={styles.location}>London</Text>
-        <Text>6</Text>
-        <Text>Feels like 5</Text>
-
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <UpcomingWeather />
+    </View>
   );
 }
-
-export default App;
 
 const styles = StyleSheet.create({ // stylesheets are made as objects
   wrapper: {
     flex: 1
   },
-
+  
   container: {
     flex: 1,
-    backgroundColor: 'darkgrey',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
-
-  location: {
-    fontSize: 20,
-    fontWeight: 700,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
 });
+
+export default App;
